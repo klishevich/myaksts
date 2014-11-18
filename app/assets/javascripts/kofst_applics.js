71d50d8e35cddbcad3eb90a2bd0f438402f668a0
klisha <mans1@ngs.ru>
2014-11-18 08:58:23 +0300

    add dependent science specific field select

--- app/assets/javascripts/kofst_applics.js
+++ app/assets/javascripts/kofst_applics.js
@@ -1,5 +1,25 @@
 function update_science_specific_field(element){
-    alert(element.value);
+//    alert(element.value);
+  var $el = $("#kofst_applic_science_specific_field_id");
+  $el.empty(); // remove old options
+  $.getJSON("/science_specific_fields/index?term=" + element.value,function(result){
+    $.each(result, function(key, value){
+       $el.append($("<option></option>")
+          .attr("value", value.id).text(value.name));
+    });
+  });    
+
+    // var newOptions = {"Option 1": "value1",
+    //   "Option 2": "value2",
+    //   "Option 3": "value3"
+    // };
+
+    // var $el = $("#kofst_applic_science_specific_field_id");
+    // $el.empty(); // remove old options
+    // $.each(newOptions, function(key, value) {
+    //   $el.append($("<option></option>")
+    //      .attr("value", value).text(key));
+    // });    
 
 };
 
