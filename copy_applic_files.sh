5a578b56ec0a5829e1427f8e424516389b6d578f
Michael Klishevich <busation@gmail.com>
2018-06-17 19:39:15 +0300

    add copy script

--- copy_applic_files.sh
+++ copy_applic_files.sh
@@ -1,3 +1,5 @@
-ssh jupiter.j123.ru 'cd ~/apps/myaksts/shared/system && zip -r applic_files.zip applic_files'
-scp jupiter.j123.ru:~/apps/myaksts/shared/system/applic_files.zip /Users/mike/Downloads/
-ssh jupiter.j123.ru 'rm ~/apps/myaksts/shared/system/applic_files.zip'
+d=$(date +%Y%m%d)
+echo $d
+ssh jupiter.j123.ru "cd ~/apps/myaksts/shared/system && zip -r applic_files$d.zip applic_files"
+scp jupiter.j123.ru:"~/apps/myaksts/shared/system/applic_files$d.zip" /Users/mike/Downloads/
+ssh jupiter.j123.ru "rm ~/apps/myaksts/shared/system/applic_files$d.zip"
